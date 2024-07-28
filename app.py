f4d6bc15ae8685b5ffeaba95ded929645e039fcf
BorislavSergev <borislavsergev18@gmail.com>
2024-07-28 14:34:41 +0300

    Port update

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ socketio = SocketIO(app)
 setup_routes(app, socketio)
 
 if __name__ == '__main__':
-    socketio.run(app,host='0.0.0.0', port='5051', debug=True)
+    socketio.run(app,host='0.0.0.0', port='5050', debug=True)
